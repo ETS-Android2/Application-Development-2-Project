00dc9177c5a53e316882481323a070e038ee1b51
Chilka Castro <chilcjcastro@yahoo.ca>
2022-04-16 02:02:28 -0400

    Replacing the old Mood Planet project

---
+++ Mood Planet/app/src/main/java/com/example/moodplanet/RegisterUser.java
@@ -0,0 +1,157 @@
+package com.example.moodplanet;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class RegisterUser extends AppCompatActivity implements View.OnClickListener {
+    private TextView banner, registerUser;
+    private EditText editTextFirstName, editTextLastName, editTextEmail, editTextPassword;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+
+        mAuth = FirebaseAuth.getInstance();
+        banner = (TextView) findViewById(R.id.banner);
+        banner.setOnClickListener(this);
+
+        registerUser = (Button) findViewById(R.id.registerUser);
+        registerUser.setOnClickListener(this);
+
+        editTextFirstName = (EditText) findViewById(R.id.firstName);
+        editTextFirstName.setOnClickListener(this);
+
+        editTextLastName = (EditText) findViewById(R.id.lastName);
+        editTextLastName.setOnClickListener(this);
+
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextEmail.setOnClickListener(this);
+
+        editTextPassword = (EditText) findViewById(R.id.password);
+        editTextPassword.setOnClickListener(this);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.banner:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.registerUser:
+                registerUser();
+                break;
+        }
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String firstName = editTextFirstName.getText().toString().trim();
+        String lastName = editTextLastName.getText().toString().trim();
+
+        if (firstName.isEmpty() && lastName.isEmpty() && email.isEmpty() && password.isEmpty()) {
+            editTextFirstName.setError("First Name is required!");
+            editTextLastName.setError("Last Name is required!");
+            editTextEmail.setError("Email is required!");
+            editTextPassword.setError("Password is required!");
+            return;
+        }
+
+        if (firstName.isEmpty()) {
+            editTextFirstName.setError("First Name is required!");
+            editTextFirstName.requestFocus();
+            return;
+        }
+
+        if (lastName.isEmpty()) {
+            editTextLastName.setError("Last Name is required!");
+            editTextLastName.requestFocus();
+            return;
+        }
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please provide valid email!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password is required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            editTextPassword.setError("Minimum password length should be 6 characters");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            User user = new User(firstName, lastName, email);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(RegisterUser.this,
+                                                "User has been registered successfully", Toast.LENGTH_LONG).show();
+                                        progressBar.setVisibility(View.VISIBLE);
+
+                                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                    }
+                                    else {
+                                        Toast.makeText(RegisterUser.this,
+                                                "Failed to register! Try again!", Toast.LENGTH_LONG).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+                        }
+                        else {
+                            Toast.makeText(RegisterUser.this,
+                                    "Failed to register! Try again!", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+    }
+}
---
+++ Mood Planet/app/src/main/java/com/example/moodplanet/User.java
@@ -0,0 +1,20 @@
+package com.example.moodplanet;
+
+/**
+ * Created by Chilka Castro on 4/16/2022.
+ */
+public class User {
+    public String firstName, lastName, email;
+
+    public User() {
+
+    }
+
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+
+}
