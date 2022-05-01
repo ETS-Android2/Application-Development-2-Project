3a473b9b461e3d5ba6dcbc340b1cbc65e4756465
Phuong Thanh Nguyen <phuongthanhnguyen136@gmail.com>
2022-05-01 18:53:37 -0400

    adding chart file and examples

--- Mood Planet/app/src/main/java/com/example/moodplanet/ChartMainActivity.java
+++ Mood Planet/app/src/main/java/com/example/moodplanet/ChartMainActivity.java
@@ -2,33 +2,117 @@ package com.example.moodplanet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.anychart.APIlib;
 import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
 import com.anychart.chart.common.dataentry.DataEntry;
 import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
 import com.anychart.charts.Pie;
+import com.anychart.core.cartesian.series.Column;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.HoverMode;
+import com.anychart.enums.Position;
+import com.anychart.enums.TooltipPositionMode;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ChartMainActivity extends AppCompatActivity {
 
+    ArrayList barArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart_main);
-        Pie pie = AnyChart.pie();
 
-        List<DataEntry> data = new ArrayList<>();
-        data.add(new ValueDataEntry("John", 10000));
-        data.add(new ValueDataEntry("Jake", 12000));
-        data.add(new ValueDataEntry("Peter", 18000));
+        /**
+         * bar chart example from any chart
+         */
+//        AnyChartView anyChartView = findViewById(R.id.any_chart_view);
+//        anyChartView.setProgressBar(findViewById(R.id.progress_bar));
+//
+//        Cartesian cartesian = AnyChart.column();
+//
+//        List<DataEntry> data = new ArrayList<>();
+//        data.add(new ValueDataEntry("Rouge", 80540));
+//        data.add(new ValueDataEntry("Foundation", 94190));
+//        data.add(new ValueDataEntry("Mascara", 102610));
+//        data.add(new ValueDataEntry("Lip gloss", 110430));
+//        data.add(new ValueDataEntry("Lipstick", 128000));
+//        data.add(new ValueDataEntry("Nail polish", 143760));
+//        data.add(new ValueDataEntry("Eyebrow pencil", 170670));
+//        data.add(new ValueDataEntry("Eyeliner", 213210));
+//        data.add(new ValueDataEntry("Eyeshadows", 249980));
+//
+//        Column column = cartesian.column(data);
+//
+//        column.tooltip()
+//                .titleFormat("{%X}")
+//                .position(Position.CENTER_BOTTOM)
+//                .anchor(Anchor.CENTER_BOTTOM)
+//                .offsetX(0d)
+//                .offsetY(5d)
+//                .format("${%Value}{groupsSeparator: }");
+//
+//        cartesian.animation(true);
+//        cartesian.title("Top 10 Cosmetic Products by Revenue");
+//
+//        cartesian.yScale().minimum(0d);
+//
+//        cartesian.yAxis(0).labels().format("${%Value}{groupsSeparator: }");
+//
+//        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+//        cartesian.interactivity().hoverMode(HoverMode.BY_X);
+//
+//        cartesian.xAxis(0).title("Product");
+//        cartesian.yAxis(0).title("Revenue");
+//
+//        anyChartView.setChart(cartesian);
 
-        pie.data(data);
+//        AnyChartView anyChartView1 = findViewById(R.id.any_chart_view2);
+//
+//        Pie pie1 = AnyChart.pie();
+//
+//        List<DataEntry> data1 = new ArrayList<>();
+//        data1.add(new ValueDataEntry("Apples", 6371664));
+//        data1.add(new ValueDataEntry("Pears", 789622));
+//        data1.add(new ValueDataEntry("Bananas", 7216301));
+//        data1.add(new ValueDataEntry("Grapes", 1486621));
+//        data1.add(new ValueDataEntry("Oranges", 1200000));
+//
+//        pie1.data(data1);
+//
+//        anyChartView1.setChart(pie1);
 
-        AnyChartView anyChartView = (AnyChartView) findViewById(R.id.any_chart_view);
-        anyChartView.setChart(pie);
+//        BarChart barChart = findViewById(R.id.barchart);
+//
+//        getData();
+//
+//        BarDataSet barDataSet = new BarDataSet(barArrayList, "Bar Chart");
+//        BarData barData = new BarData(barDataSet);
+//        barChart.setData(barData);
+//        barDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+//        barDataSet.setValueTextColor(Color.BLACK);
+//        barDataSet.setValueTextSize(16f);
+//        barChart.getDescription().setEnabled(true);
     }
+
+//    private void getData() {
+//        barArrayList = new ArrayList();
+//
+//        barArrayList.add(new BarEntry(2f,10));
+//        barArrayList.add(new BarEntry(3f,20));
+//        barArrayList.add(new BarEntry(4f,30));
+//        barArrayList.add(new BarEntry(5f,40));
+//        barArrayList.add(new BarEntry(6f,50));
+//    }
 }
